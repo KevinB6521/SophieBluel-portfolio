c9fba547ca1ac1722c8af0a45c35c9928cceb0f0
Kevin Barre <kevinbay105@gmail.com>
2023-03-13 17:40:04 +0100

    Ajout d'une boucle for dans la fonction getWorks pour récupérer les images et les textes

--- Backend/works.js
+++ Backend/works.js
@@ -2,5 +2,14 @@ async function getWorks() {
     let url = "http://localhost:5678/api/works";
 
     let response = await fetch(url);
-    return await response.json();
+    let works = await response.json();
+
+    for (let i = 0; i < works.length; i++) {
+        const imageElement = document.querySelector("img");
+        const titleElement = document.querySelector("figcaption");
+
+        imageElement.src = works[i].imageUrl;
+
+        
+    }
 };
