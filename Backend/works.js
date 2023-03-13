c4681fc3a6b36df73b0ab78683396030327fb2cd
Kevin Barre <kevinbay105@gmail.com>
2023-03-13 17:28:54 +0100

    Appel à la fonction fetch pour récupérer les travaux de l'architecte depuis l'API

--- Backend/works.js
+++ Backend/works.js
@@ -1,1 +1,6 @@
-fetch("http://localhost:5678/api/works");
+async function getWorks() {
+    let url = "http://localhost:5678/api/works";
+
+    let response = await fetch(url);
+    return await response.json();
+};
